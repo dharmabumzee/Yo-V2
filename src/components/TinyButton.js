47e56ae13635d52436d086a8097e253e39d5d5de
lukabelani <luka.belani@gmail.com>
2020-09-27 22:25:37 +0200

    add scroll to top animated arrow

---
+++ src/components/TinyButton.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { TinyButton as ScrollUpButton } from "react-scroll-up-button";
+
+class TinyButton extends React.Component {
+  render() {
+    return (
+      <ScrollUpButton
+        EasingType="easeInOutQuad"
+        StopPosition={0}
+        ShowAtPosition={550}
+        AnimationDuration={1500}
+        className="tinybutton"
+      />
+    );
+  }
+}
+
+export default TinyButton;
