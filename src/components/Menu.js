411ac546343a23c3d9c86661b162dda9f2853827
lukabelani <luka.belani@gmail.com>
2020-09-27 22:26:03 +0200

    create menu

---
+++ src/components/Menu.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Menu = () => {
+  return (
+    <div id="menu">
+      <a href="/">APOD</a>
+      <a href="/images">IMAGES</a>
+    </div>
+  );
+};
+
+export default Menu;
